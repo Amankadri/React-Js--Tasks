bd169b3e9bad8126a440480d87de59f85bf3c218
Amankadri <kadriaman2@gmail.com>
2023-08-04 19:03:03 +0530

    ReactJs

--- netninja-pratice/src/App.js
+++ netninja-pratice/src/App.js
@@ -1,16 +1,21 @@
-
-import './App.css';
+// import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
-  const link = "https//www.google.com";
+  const link = "https://www.youtube.com/";
+  // const aman = [{name: "aman", age: "20"}];   react doesn't allows object values;
+  //const popo = true;     Same goes for boolean values in react
   return (
     <div className="App">
+      <Navbar/>
       <div className='content'>
+        <Home/>
         <h1>{title} </h1>
         <p>Liked {likes} times</p>
-        <a href={link}>Google</a>
+        <a href={link}>Youtube</a>
         <p>{Math.random() * 10}</p>
       </div>
 
