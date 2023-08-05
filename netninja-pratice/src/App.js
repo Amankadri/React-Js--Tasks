66f07d322a21cdfbe0daa41406a6a5040fc57a92
Amankadri <kadriaman2@gmail.com>
2023-08-05 11:03:23 +0530

    ReactJs

--- netninja-pratice/src/App.js
+++ netninja-pratice/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
-  const link = "https//www.google.com";
+  const link = "https://www.youtube.com/";
   return (
     <div className="App">
       <div className='content'>
         <h1>{title} </h1>
         <p>Liked {likes} times</p>
-        <a href={link}>Google</a>
+        <a href={link}>Youtube</a>
         <p>{Math.random() * 10}</p>
       </div>
 
