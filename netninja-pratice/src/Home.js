67413d5db640e7828c82415614071a885e676bd2
Amankadri <kadriaman2@gmail.com>
2023-08-07 17:40:04 +0530

    ReactJs

--- netninja-pratice/src/Home.js
+++ netninja-pratice/src/Home.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import BlogList from "./blog-list";
+
 const Home = () => {
+   
+    const [blog, setBlog]= useState([
+        { title: 'My new website', body: 'lorem ipsum....', author: 'Aman', id:'1'},
+        { title: 'Welcome party', body: 'lorem ipsum....', author: 'Batman', id:'2'},
+        { title: 'Web dev top tips', body: 'lorem ipsum....', author: 'Aman', id:'3'}
+    ])
+
+    const handleDelete = (id) =>{
+        const newBlogs= blog.filter(blog => blog.id !== id);
+        setBlog(newBlogs);
+
+    }
+  const[fullname, setName]= useState('Aman');
+  const[age, setAge]= useState(20);
+
     const handleClick= () =>
     {
+        setName('Batman');
+        setAge(30)
         console.log("Hello")
     }
 
@@ -12,6 +32,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h1>Homepage</h1>
+            <p>{fullname} is {age} years old  </p>
             <button onClick={handleClick} style ={{
                     color: "crimson",
                     backgroundColor: "white",
@@ -22,7 +43,12 @@ const Home = () => {
             {
                 handleClickAgain(" Aman")
             }}>Click me again</button>
+
+        <BlogList blogs={blog} title="All Blogs!" handleDelete={handleDelete}/>
+        <BlogList blogs={blog.filter((blog)=> blog.author==='Aman')} title="Aman's Blogs!"/>
         </div>
+
+        
      );
 }
  
